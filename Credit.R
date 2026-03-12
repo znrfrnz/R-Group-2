3fb53c2096026c240637615c0fa24b387e6f34c6
Juliana-Eunice <julianaeuniceaastibe@iskolarngbayan.pup.edu.ph>
2026-03-13 01:29:23 +0800

    I added my part (ggplots). Please check and tell me if there are any suggestions. I mostly followed the R recording and the given R script.

--- Credit.R
+++ Credit.R
@@ -1,12 +1,15 @@
 #BSIT 2-1 | GROUP 2 | ASSIGNMENT #2
 
 #----Load necessary libraries-----
-library(readxl) #to read the excel file
+library(readxl) # to read the excel file
 library(dplyr) # for data manipulation
-library(ggplot2) #to visualize the data
+library(ggplot2) # to visualize the data
 
 #-----DATA-----
 German_Credit_Risk <- read_excel("C:/Users/Jenny/Downloads/German Credit Risk.xlsx")
+View(German_Credit_Risk)
+DATA <- German_Credit_Risk
+attach(DATA)
 
 #-----Task 1 – Custom Function-----
 # Create a custom R function (e.g., a function that calculates credit risk metrics or summarizes loan info) and demonstrate its use with examples.
@@ -29,3 +32,29 @@ German_Credit_Risk <- read_excel("C:/Users/Jenny/Downloads/German Credit Risk.xl
 
 #-----Task 5 – ggplot2 Visualizations-----
 # Create meaningful visualizations of the German Credit Dataset using ggplot2 only
+ggplot(DATA, aes(x = Age, y = Credit_amount)) + 
+  
+  # Points
+  geom_point(aes(color = Risk), alpha = 0.6) +
+
+  # Color of points
+  scale_color_manual(values = c("bad" = "plum2", "good" = "mediumorchid")) +
+  
+  # Smooth regression line
+  geom_smooth(method = "lm", se = FALSE, color = "darkorchid4") +
+
+  # Facet by purpose
+  facet_wrap(~Purpose) +
+    
+  # Customize theme
+  theme_minimal() +
+  theme(
+    plot.title = element_text(face = "bold"),
+    strip.text = element_text(face = "bold"),
+    legend.position = "bottom") +
+  
+  # Titles and labels
+  labs(title = "Age vs. Credit Amount",
+       subtitle = "Colored by Risk and Faceted by Purpose",
+       x = "Applicant Age",
+       y = "Credit Amount") 
