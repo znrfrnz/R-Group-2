371bedb87b99ae8024afa852935b3eceb218ed05
znrfrnz <franzbaes69@gmail.com>
2026-03-12 16:42:50 +0800

    Initial Commit

---
+++ Credit.R
@@ -0,0 +1,21 @@
+# Task 1 – Custom Function
+# Create a custom R function (e.g., a function that calculates credit risk metrics or summarizes loan info) and demonstrate its use with examples.
+
+
+
+# Task 2 – Data Structures
+# Create and assign all required data structures: a vector, an ordered factor, an unordered factor, a table, and a data frame (using the German Credit Dataset where applicable).
+
+
+
+# Task 3 – Data Manipulation with dplyr
+
+
+
+# Task 4 – Base R Visualizations
+# Create meaningful visualizations of the German Credit Dataset using Base R graphics only
+
+
+
+# Task 5 – ggplot2 Visualizations
+# Create meaningful visualizations of the German Credit Dataset using ggplot2 only
