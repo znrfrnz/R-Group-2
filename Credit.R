0be7b98b9173723080da1577c565695ba12a9319
Juliana-Eunice <julianaeuniceaastibe@iskolarngbayan.pup.edu.ph>
2026-03-13 00:04:17 +0800

    Added libraries and inserted the excel file.

--- Credit.R
+++ Credit.R
@@ -1,23 +1,30 @@
-# Tasking
+#BSIT 2-1 | GROUP 2 | ASSIGNMENT #2
 
-# Task 1 – Custom Function
+#----Load necessary libraries-----
+library(readxl) #to read the excel file
+library(ggplot2) #to visualize the data
+
+#-----DATA-----
+German_Credit_Risk <- read_excel("C:/Users/Jenny/Downloads/German Credit Risk.xlsx")
+
+#-----Task 1 – Custom Function-----
 # Create a custom R function (e.g., a function that calculates credit risk metrics or summarizes loan info) and demonstrate its use with examples.
 
 
 
-# Task 2 – Data Structures
+#-----Task 2 – Data Structures-----
 # Create and assign all required data structures: a vector, an ordered factor, an unordered factor, a table, and a data frame (using the German Credit Dataset where applicable).
 
 
 
-# Task 3 – Data Manipulation with dplyr
+#-----Task 3 – Data Manipulation with dplyr----
 
 
 
-# Task 4 – Base R Visualizations
+#-----Task 4 – Base R Visualizations-----
 # Create meaningful visualizations of the German Credit Dataset using Base R graphics only
 
 
 
-# Task 5 – ggplot2 Visualizations
+#-----Task 5 – ggplot2 Visualizations-----
 # Create meaningful visualizations of the German Credit Dataset using ggplot2 only
